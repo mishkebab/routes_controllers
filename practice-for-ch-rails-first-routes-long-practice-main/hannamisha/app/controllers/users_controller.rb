8a7643a224535698024a9ed2e618dd351f613d34
hannadarwish <hannadarwish@gmail.com>
2023-05-03 16:29:10 -0700

    finished project

--- practice-for-ch-rails-first-routes-long-practice-main/hannamisha/app/controllers/users_controller.rb
+++ practice-for-ch-rails-first-routes-long-practice-main/hannamisha/app/controllers/users_controller.rb
@@ -13,11 +13,28 @@ class UsersController < ApplicationController
         end
     end
 
-    def user_params
-        params.require(:banana).permit(:name, :email)
-    end 
-
     def show
-        render json: params
+        @user = User.find(params[:id])
+        render json: @user
     end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            redirect_to user_url(@user)
+        else
+            render json: @user.errors.full_messages, status: 422
+        end
+    end
+
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        render json: @user
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:name, :email)
+    end 
 end
