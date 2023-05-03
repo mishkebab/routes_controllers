b4ee3eee336e2e772e1c75abc80ee25e14baa690
misha <mishabansal7@gmail.com>
2023-05-03 16:06:57 -0700

    started users controller

--- practice-for-ch-rails-first-routes-long-practice-main/hannamisha/app/controllers/users_controller.rb
+++ practice-for-ch-rails-first-routes-long-practice-main/hannamisha/app/controllers/users_controller.rb
@@ -1,5 +1,23 @@
 class UsersController < ApplicationController
     def index
-        render plain: "I'm in the index action!"
+        @users = User.all
+        render json: @users
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            render json: @user
+        else 
+            render json: @user.errors.full_messages, status: 422
+        end
+    end
+
+    def user_params
+        params.require(:banana).permit(:name, :email)
+    end 
+
+    def show
+        render json: params
     end
 end
