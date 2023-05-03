534fa84aae93ed8a60b7bd859005bb077c536887
misha <mishabansal7@gmail.com>
2023-05-03 14:13:14 -0700

    finished making routes

---
+++ practice-for-ch-rails-first-routes-long-practice-main/hannamisha/config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  # resources :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+  get '/users', to: 'users#index', as: "users"
+  post '/users', to: 'users#create'
+  get '/users/:id', to: 'users#show', as: "user"
+  patch '/users/:id', to: 'users#update'
+  put '/users/:id', to: 'users#update'
+  delete '/users/:id', to: 'users#destroy'
+  get '/users/new', to: 'users#new', as: "new_user"
+  get '/users/:id/edit', to: 'users#edit', as: "edit_user"
+end
