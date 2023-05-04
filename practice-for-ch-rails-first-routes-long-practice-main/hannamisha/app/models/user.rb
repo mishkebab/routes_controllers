9eec7768832ad2ab055b79c0e7bde6a3ea3db0ce
misha <mishabansal7@gmail.com>
2023-05-04 09:18:01 -0700

    testing

--- practice-for-ch-rails-first-routes-long-practice-main/hannamisha/app/models/user.rb
+++ practice-for-ch-rails-first-routes-long-practice-main/hannamisha/app/models/user.rb
@@ -3,9 +3,10 @@
 # Table name: users
 #
 #  id         :bigint           not null, primary key
+#  name       :string           not null
+#  email      :string           not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
-#  username   :string           not null
 #
 class User < ApplicationRecord
     validates :username, presence: true, uniqueness: true
