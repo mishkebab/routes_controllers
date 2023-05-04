506503111d737580235a594000d74f15dc1b1f5f
hannadarwish <hannadarwish@gmail.com>
2023-05-04 09:50:27 -0700

    finished phase 1

--- practice-for-ch-rails-first-routes-long-practice-main/hannamisha/app/models/artwork.rb
+++ practice-for-ch-rails-first-routes-long-practice-main/hannamisha/app/models/artwork.rb
@@ -18,4 +18,13 @@ class Artwork < ApplicationRecord
     foreign_key: :artist_id,
     class_name: :User
 
+    has_many :artwork_shares,
+    foreign_key: :artwork_id,
+    class_name: :ArtworkShare,
+    dependent: :destroy
+
+    has_many :shared_viewers,
+    through: :artwork_shares,
+    source: :viewer
+
 end 
--- practice-for-ch-rails-first-routes-long-practice-main/hannamisha/db/seeds.rb
+++ practice-for-ch-rails-first-routes-long-practice-main/hannamisha/db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Destroying tables..."
+
+  User.destroy_all
+  Artwork.destroy_all
+  ArtworkShare.destroy_all
+
+  puts "Resetting primary keys..."
+  # For easy testing, so that after seeding, the first `User` has `id` of 1
+  ["users", "artworks", "artwork_shares"].each do |table_name|
+    ApplicationRecord.connection.reset_pk_sequence!(table_name)
+  end
+
+  puts "Creating users..."
+
+User.create(username: "hanna")
+User.create(username: "misha")
+User.create(username: "da vinci")
+User.create(username: "thomas")
+
+Artwork.create(title: "mona lisa", image_url: "mona lisa url", artist_id: 3)
+Artwork.create(title: "beautiful ruby", image_url: "beautiful ruby url", artist_id: 2)
+Artwork.create(title: "bananable", image_url: "bananable url", artist_id: 1)
+
+ArtworkShare.create(artwork_id: 1, viewer_id: 4)
+ArtworkShare.create(artwork_id: 1, viewer_id: 2)
+ArtworkShare.create(artwork_id: 2, viewer_id: 1)
+ArtworkShare.create(artwork_id: 3, viewer_id: 4)
+ArtworkShare.create(artwork_id: 1, viewer_id: 3)
